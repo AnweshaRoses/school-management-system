8e7c99cd1716d18830d9459a7ff70a182fe01903
meghna70 <meghnabal70@gmail.com>
2022-04-23 17:43:37 +0530

    event home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
 import React from 'react';
 import './App.css';
-// import Navbar from './components/NavBar';
 import Sidebar from './components/sidebar';
-
+import Events from './pages/events';
+import Login from './pages/login-in';
+import Home  from './pages/home';
 export default function App(){
+
+
   return(
-    <Sidebar/>
-  )
+    <div className='App'>
+          <Sidebar/>
+          <div className='secondary'>
+          {(window.location.pathname=="/Events")
+              && <Events/>}
+          {/* {(window.location.pathname=="/")
+              && <Login/>} */}
+           {(window.location.pathname=="/home")
+              && <Home pic='https://media.istockphoto.com/photos/portrait-of-happy-middle-school-boy-picture-id1138949949?k=20&m=1138949949&s=170667a&w=0&h=u2ArzGTmMPObeKDbgGx3Pe7RJMQzXtk_j_JpMc21z5k='
+                       name=" Tammy Mishra "
+                       class="8"
+                       section="E"
+                       roll="34567"
+                       teacher="John Doe"
+              />}
+          </div>
+
+    </div>    
+    )
 }
 
 
 
+// import { BrowserRouter as Router, Routes, Route}
+// 	from 'react-router-dom';
+// import Home from './pages';
+// import About from './pages/about';
+// import Blogs from './pages/blogs';
+// import SignUp from './pages/signup';
+// import Login from './pages/login-in';
+
+// function App() {
+// return (
+// 	<Router>
+// 	<Navbar />
+// 	<Routes>
+// 		<Route exact path='/' exact element={<Home />} />
+// 		<Route path='/about' element={<About/>} />
+// 		<Route path='/contact' element={<Contact/>} />
+// 		<Route path='/blogs' element={<Blogs/>} />
+// 		<Route path='/sign-up' element={<SignUp/>} />
+// 	</Routes>
+// 	</Router>
+// );
+// }
+
+// export default App;
--- src/components/Data-sidebar.js
+++ src/components/Data-sidebar.js
@@ -2,10 +2,12 @@ import HomeIcon from '@mui/icons-material/Home';
 import QuizIcon from '@mui/icons-material/Quiz';
 import AttendanceIcon from '@mui/icons-material/CoPresent';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import EventIcon from '@mui/icons-material/Festival';
 import React from "react"
 
  export const SidebarData = [
-    {
+    {  
+
         title:"Home",
         icon:<HomeIcon/>,
         link:"/home"
@@ -21,9 +23,58 @@ import React from "react"
         link:"/attendance"
      },
      {
+      title:"Events",
+      icon:<EventIcon/>,
+      link:"/Events"
+   },
+     {
         title:"Calender",
         icon:<CalendarMonthIcon/>,
         link:"/calender"
      }
  ]
-      
+  
+ 
+
+ export const EventsData = [
+
+   {
+       title:"Sports Day",
+       date:"12th August 2022",
+       line1:"Sports day of Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+       line2: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam",
+       img:"https://www.cedarcollege.sa.edu.au/wp-content/uploads/2017/03/Sports-day1.jpg"
+    },
+
+    {
+      title:"Home",
+      date:"12th August 2022",
+      line1:"hello",
+      line2:"world",
+      img:""
+   },
+
+   {
+      title:"Home",
+      date:"12th August 2022",
+      line1:"hello",
+      line2:"world",
+      img:""
+   },
+
+   {
+      title:"Home",
+      date:"12th August 2022",
+      line1:"hello",
+      line2:"world",
+      img:""
+   }
+    
+]
+
+// export const subjectsData = [
+//      "biology",
+//      "chemistry",
+//      "physics",
+//      "maths"
+// ]
