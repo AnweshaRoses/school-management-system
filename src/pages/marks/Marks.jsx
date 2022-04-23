eb83776d022bcccb235e75d4cf28e7fdbb19b62a
meghna70 <meghnabal70@gmail.com>
2022-04-23 19:33:10 +0530

    Update Marks.jsx

--- src/pages/marks/Marks.jsx
+++ src/pages/marks/Marks.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Charts from '../../components/chart/Charts'
 import { marksData } from '../../components/Data-sidebar'
 import Featured from '../../components/featured/Featured'
 
@@ -8,7 +7,7 @@ export const Marks = () => {
   return (
     <div className='marks'>
         <Featured/>
-        <Charts data={marksData} title="User Analytics" grid dataKey="users"/>
+        
     </div>
   )
 }
