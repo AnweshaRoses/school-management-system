65403b3339434b766ffd5546be7dbdb89952cd90
Anwesha Mishra <anwesharoses@gmail.com>
2022-04-23 23:48:42 +0530

    trying to integrate

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 export default function LoginForm() {
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    fetch("http://localhost:5000/api/auth/login")
+  }
   return (
-    <form action="/login">
+    <form action="/login" onSubmit={handleSubmit}>
         <label for="email">Email</label>
         <input type="email" id="email" />
         <label for="password">Password</label>
